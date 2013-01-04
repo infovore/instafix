c2f3339e5007110060d23edef8f0e55187b867c5
Tom Armitage <tom@infovore.org>
2013-01-04 11:45:00 +0000

    Refactoring this.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,21 +9,10 @@ class PhotosController < ApplicationController
     #render :text => params[:photos]
     params["photos"].each do |id, photo_hash|
       actual_photo = Photo.find(id)
-      # update attributes
-      actual_photo.title = photo_hash["title"]
-      actual_photo.description = photo_hash["description"]
-      actual_photo.taglist = photo_hash["taglist"]
+      actual_photo.update_from_photo_hash(photo_hash)
+      
       # update flickr
-      #flickr.photos.setMeta(:photo_id => actual_photo.flickr_id,
-                            #:title => actual_photo.title,
-                            #:description => actual_photo.description)
-      #flickr.photo_id.setTags(:photo_id => actual_photo.flickr_id,
-                              #:tags => actual_photo.taglist)
-      # update hidden
-      if photo_hash["is_hidden"] == "1" 
-        actual_photo.is_hidden = true
-      end
-      actual_photo.save
+      #actual_photo.update_on_flickr_for_user(current_user)
     end
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,33 @@ class Photo < ActiveRecord::Base
   scope :visible, :conditions => {:is_hidden => false}
   scope :hidden, :conditions => {:is_hidden => true}
 
+  def update_from_photo_hash(photo_hash)
+    # update attributes
+    title = photo_hash["title"]
+    description = photo_hash["description"]
+    taglist = photo_hash["taglist"]
+
+    # update hidden
+    if photo_hash["is_hidden"] == "1" 
+      is_hidden = true
+    end
+    save
+  end
+
+  def update_on_flickr_for_user(user)
+    FlickRaw.api_key = FLICKR_API_KEY
+    FlickRaw.shared_secret = FLICKR_SHARED_SECRET
+    flickr = FlickRaw::Flickr.new
+    flickr.access_token = user.oauth_token
+    flickr.access_secret = user.oauth_token_secret
+
+    flickr.photos.setMeta(:photo_id => self.flickr_id,
+                          :title => self.title,
+                          :description => self.description)
+    flickr.photos.setTags(:photo_id => self.flickr_id,
+                            :tags => self.taglist)
+  end
+
   def self.ingest_latest_for_user(user)
     FlickRaw.api_key = FLICKR_API_KEY
     FlickRaw.shared_secret = FLICKR_SHARED_SECRET
